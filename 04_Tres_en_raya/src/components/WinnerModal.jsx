fdd03aa2ea6c1243d74de00098b9406495098923
javiermallen <javiermallen@yahoo.es>
2023-02-06 10:49:20 +0100

    Eslint

--- 04_Tres_en_raya/src/components/WinnerModal.jsx
+++ 04_Tres_en_raya/src/components/WinnerModal.jsx
@@ -1,22 +1,20 @@
-import { Square } from "./Square"
+import { Square } from './Square'
 
 export const WinnerModal = ({ winner, resetGame }) => {
-    if ( winner === null ) return null
-    const winnerText = winner === false ? 'Empate' : `El ganador es:`
+  if (winner === null) return null
+  const winnerText = winner === false ? 'Empate' : 'El ganador es:'
 
-    return (
-        <section className="winner">
-            <div className="text">
-                <h2> { winnerText }</h2>
-                <header className="win">
-                    { winner && <Square>{ winner }</Square>}
-                </header>
-                <footer>
-                    <button onClick={ resetGame }>Volver a comenzar</button>                
-                </footer>            
-            </div>
-        </section>  
-    )
+  return (
+    <section className='winner'>
+      <div className='text'>
+        <h2> {winnerText}</h2>
+        <header className='win'>
+          {winner && <Square>{winner}</Square>}
+        </header>
+        <footer>
+          <button onClick={resetGame}>Volver a comenzar</button>
+        </footer>
+      </div>
+    </section>
+  )
 }
-    
-             
--- 04_Tres_en_raya/src/constants.js
+++ 04_Tres_en_raya/src/constants.js
@@ -1,17 +1,17 @@
 
 export const TURNO = {
-    X: "❌",
-    O: "🟢"
+  X: '❌',
+  O: '🟢'
 }
-  
-//Posibilidades ganadoras
+
+// Posibilidades ganadoras
 export const WINNER_COMBOS = [
-    [0, 1, 2], 
-    [3, 4, 5], 
-    [6, 7, 8], 
-    [0, 3, 6], 
-    [1, 4, 7], 
-    [2, 5, 8], 
-    [0, 4, 8], 
-    [2, 4, 6]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
 ]
